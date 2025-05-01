1bb708d846dfcf458fd89da4a3048a6ffb6d3d51
thequang <nguyenthequangdev@gmail.com>
2025-05-01 15:50:11 +0700

    Hoàn thiện giao diện (1)

---
+++ src/pages/TypeProductPage/TypeProductPage.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import NavbarComponent from "../../components/NavbarComponent/NavbarComponent";
+import CardComponent from "../../components/CardComponent/CardComponent";
+import { Col, Pagination, Row } from "antd";
+import { WrapperNabar, WrapperProducts } from "./style";
+
+function TypeProductPage() {
+    const onChange = () => {
+
+    }
+
+    return (
+        <div style={{ padding: '0 120px', background: '#efefef'}}>
+            <Row style={{ flexWrap: 'nowrap', paddingTop: '10px' }}>
+                <WrapperNabar span={4}>
+                    <NavbarComponent />
+                </WrapperNabar>
+
+                <Col span={20}>
+                    <WrapperProducts span={20}>
+                        <CardComponent />
+                        <CardComponent />
+                        <CardComponent />
+                        <CardComponent />
+                        <CardComponent />
+                    </WrapperProducts>
+
+                    <Pagination defaultCurrent={2} total={100} onChange={onChange} style={{
+                        justifyContent: 'center', marginTop: '10px'
+                    }}/>
+                </Col>
+            </Row>
+        </div>
+    );
+}
+
+export default TypeProductPage;
---
+++ src/pages/TypeProductPage/style.js
@@ -0,0 +1,18 @@
+import { Col } from "antd";
+import styled from "styled-components";
+
+export const WrapperProducts = styled.div`
+    display: flex;
+    justify-content: center;
+    gap: 15px;
+    margin-top: 20px;
+    flex-wrap: wrap;
+`
+export const WrapperNabar = styled(Col)`
+    background-color: #fff;
+    margin-right: 10px;
+    padding: 10px;
+    border-radius: 4px;
+    height: fit-content;
+    margin-top: 20px;
+`
